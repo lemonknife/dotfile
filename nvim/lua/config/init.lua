3bb1be206e53e836902f72469b8aa70b2607e9d4
lemonknife <linwentao99@gmail.com>
2023-08-22 15:33:58 +0800

    modify util.lua

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -2,4 +2,4 @@
 -- mapping
 util = require("core.util")
 util.set_vim(require("config.setting"))
-util.set_map("general")
+util.set_map()
--- nvim/lua/core/util.lua
+++ nvim/lua/core/util.lua
@@ -5,22 +5,20 @@ M.echo = function(str)
 	vim.api.nvim_echo({ { str, "Bold" } }, true, {})
 end
 
-M.set_map = function(section)
+M.set_map = function()
 	map = require("core.map")
-	for mode, mode_values in pairs(map[section]) do
+	for mode, mode_values in pairs(map.general) do
 		local modes = {}
 		for char in mode:gmatch(".") do
 			table.insert(modes, char)
 		end
 
-		for _, current_mode in ipairs(modes) do
 			for mapping, keybind in pairs(mode_values) do
 				local actual_key = type(keybind) == "table" and keybind[1] or keybind
 				local options = type(keybind) == "table" and keybind[2] or { noremap = true, silent = true }
-				vim.keymap.set(current_mode, mapping, actual_key, options)
+				vim.keymap.set(modes, mapping, actual_key, options)
 			end
 		end
-	end
 end
 
 M.set_vim = function(setting)
@@ -34,4 +32,23 @@ M.set_vim = function(setting)
 		end
 	end
 end
+
+M.lazy_map = function(plug)
+    map = require("core.map")
+    local keys = {}
+	for mode, mode_values in pairs(map[plug]) do
+		local modes = {}
+		for char in mode:gmatch(".") do
+			table.insert(modes, char)
+		end
+
+			for mapping, keybind in pairs(mode_values) do
+				local actual_key = type(keybind) == "table" and keybind[1] or keybind
+				local options = type(keybind) == "table" and keybind[2] or { noremap = true, silent = true }
+                table.insert(keys, { mapping, actual_key, modes, options })
+			end
+		end
+    return keys
+    end
+
 return M
