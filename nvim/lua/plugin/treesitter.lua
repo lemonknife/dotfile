8332c503956df66ed39b5a6c1e1d06a502ce1014
lemonknife <linwentao99@gmail.com>
2023-08-24 00:39:26 +0800

    modify plugin -> treesitter.lua

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -1,12 +1,13 @@
+local opts = {
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+	sync_install = false,
+	highlight = { enable = true },
+	indent = { enable = true },
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },
-	},
 	config = function()
 		require("nvim-treesitter.configs").setup(opts)
 	end,
