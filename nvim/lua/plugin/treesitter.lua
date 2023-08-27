7323699f1f31050011607e55644ebc25c771b451
lemonknife <linwentao99@gmail.com>
2023-08-27 14:46:18 +0800

    copied example treesitter config

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -1,13 +1,50 @@
-
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-opts = {
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-	sync_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },
-},
+	opts = {
+		ensure_installed = {
+			"vim",
+			"vimdoc",
+			"fish",
+			"cpp",
+			"python",
+			"json",
+			"lua",
+			"c_sharp",
+			"markdown_inline",
+			"java",
+			"latex",
+			"json5",
+			"bash",
+			"comment",
+			"bibtex",
+			"diff",
+			"toml",
+			"yaml",
+			"css",
+		},
+
+		highlight = { enable = true },
+		indent = { enable = true },
+
+		-- 不同括号颜色区分
+		rainbow = {
+			enable = true,
+			extended_mode = true,
+			max_file_lines = nil,
+		},
+
+		--启用增量选择
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<CR>",
+				node_incremental = "<CR>",
+				node_decremental = "<BS>",
+				scope_incremental = "<TAB>",
+			},
+		},
+	},
 	config = function(_, opts)
 		require("nvim-treesitter.configs").setup(opts)
 	end,
