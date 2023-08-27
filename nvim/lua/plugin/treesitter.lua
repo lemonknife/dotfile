af55a1471d7c516549966f0716cb5575e83825d0
lemonknife <linwentao99@gmail.com>
2023-08-27 15:05:04 +0800

    add new filetype -> treesitter

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -1,54 +1,54 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
-			"bash",
-			"bibtex",
-			"c_sharp",
-			"comment",
-			"cpp",
-			"css",
-			"diff",
-			"fish",
-			"java",
-			"json",
-			"json5",
-			"latex",
-			"lua",
-			"markdown_inline",
-			"python",
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+        ensure_installed = {
+            "bash",
+            "bibtex",
+            "c_sharp",
+            "comment",
+            "cpp",
+            "css",
+            "diff",
+            "fish",
+            "java",
+            "json",
+            "json5",
+            "latex",
+            "lua",
+            "markdown_inline",
+            "python",
             "scss",
-			"toml",
-			"vim",
-			"vimdoc",
-			"yaml",
+            "toml",
+            "vim",
+            "vimdoc",
+            "yaml",
             "yuck",
-		},
+        },
 
-		highlight = { enable = true },
-		indent = { enable = true },
+        highlight = { enable = true },
+        indent = { enable = true },
 
-		-- 不同括号颜色区分
-		rainbow = {
-			enable = true,
-			extended_mode = true,
-			max_file_lines = nil,
-		},
+        -- 不同括号颜色区分
+        rainbow = {
+            enable = true,
+            extended_mode = true,
+            max_file_lines = nil,
+        },
 
-		--启用增量选择
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<CR>",
-				node_incremental = "<CR>",
-				node_decremental = "<BS>",
-				scope_incremental = "<TAB>",
-			},
-		},
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+        --启用增量选择
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<CR>",
+                node_incremental = "<CR>",
+                node_decremental = "<BS>",
+                scope_incremental = "<TAB>",
+            },
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
     keys = require("util").lazy_map("treesitter"),
 }
