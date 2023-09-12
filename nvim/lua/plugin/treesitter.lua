0d1e958ac99bc174da436987d74a5e4c1f177820
lemonknife <linwentao99@gmail.com>
2023-09-12 14:53:58 +0800

    replace colorizer > coc-highlight -> nvim

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -42,20 +42,8 @@ return {
             extended_mode = true,
             max_file_lines = nil,
         },
-
-        --启用增量选择
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<CR>",
-                node_incremental = "<CR>",
-                node_decremental = "<BS>",
-                scope_incremental = "<TAB>",
-            },
-        },
     },
     config = function(_, opts)
         require("nvim-treesitter.configs").setup(opts)
     end,
-    keys = require("util").lazy_map("treesitter"),
 }
