b593efaa88a649e1bca3e6c44a0cf9daa87b6a6e
lemonknife <linwentao99@gmail.com>
2023-09-18 21:59:22 +0800

    add coc plugin list -> nvim

--- nvim/lua/plugin/coc.lua
+++ nvim/lua/plugin/coc.lua
@@ -1,7 +1,31 @@
+local util = require("util")
+
+local vim_setting = {}
+vim_setting.g = {
+    coc_global_extensions = {
+        "coc-webview",
+        "coc-sumneko-lua",
+        "coc-pairs",
+        "coc-highlight",
+        "coc-git",
+        "coc-yaml",
+        "coc-xml",
+        "coc-toml",
+        "coc-rust-analyzer",
+        "coc-pyright",
+        "coc-omnisharp",
+        "coc-ltex",
+        "coc-json",
+    },
+}
+
 return {
     "neoclide/coc.nvim",
     lazy = false,
     branch = "master",
     build = "npm ci",
-    init = require("util").set_map("coc"),
+    init = function()
+        util.set_map("coc")
+        util.set_vim(vim_setting)
+    end,
 }
