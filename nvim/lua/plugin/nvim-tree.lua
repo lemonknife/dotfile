ec9aba00e5686dfae6cfc5ddd4c84b0be9de70c7
lemonknife <linwentao99@gmail.com>
2023-09-24 11:30:05 +0800

    add nvim-tree -> nvim

---
+++ nvim/lua/plugin/nvim-tree.lua
@@ -0,0 +1,20 @@
+local util = require("util")
+local vim_setting = {}
+vim_setting.g = {
+    loaded_netrw = 1,
+    loaded_netrwPlugin = 1,
+}
+
+return {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    init = util.set_vim(vim_setting),
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    keys = util.lazy_map("tree"),
+    config = function()
+        require("nvim-tree").setup({})
+    end,
+}
