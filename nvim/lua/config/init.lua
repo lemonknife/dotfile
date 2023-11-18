cf139bd3f1a05a726997b7dc0bdf45be7be5ad86
lemonknife <linwentao99@gmail.com>
2023-11-19 00:07:43 +0800

    remove all nvim config

--- nvim/lua/config/init.lua
+++
@@ -1,8 +0,0 @@
-require("config.default")
-
-vim.api.nvim_create_autocmd("user", {
-    pattern = "VeryLazy",
-    callback = function()
-        require("config.map")
-    end,
-})
--- nvim/lua/plugin/nvim-ts-rainbow.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "p00f/nvim-ts-rainbow",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-    },
-}
