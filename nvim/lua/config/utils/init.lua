3518d0c8a5c656975be579ab96c1d5b3aac58a48
lemonknife <linwentao99@gmail.com>
2023-11-20 11:27:49 +0800

    add opt function in util -> nvim

--- nvim/lua/config/utils/init.lua
+++ nvim/lua/config/utils/init.lua
@@ -100,4 +100,13 @@ function M.has(plugin)
     return require("lazy.core.config").spec.plugins[plugin] ~= nil
 end
 
+function M.opts(name)
+    local plugin = require("lazy.core.config").plugins[name]
+    if not plugin then
+        return {}
+    end
+    local Plugin = require("lazy.core.plugin")
+    return Plugin.values(plugin, "opts", false)
+end
+
 return M
