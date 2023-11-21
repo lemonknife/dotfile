4327f9166a86f9282d08d859e4f9a1a8aceedf7d
lemonknife <linwentao99@gmail.com>
2023-11-21 17:56:55 +0800

    modify keybind -> nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,12 +1,13 @@
-local M = {}
-local mode_n = { "n", "v" }
-local mode_v = { "v" }
-local mode_i = { "i" }
-
 local map = vim.keymap.set
 
-map(mode_i, "<C-k>", "<C-j>")
-map(mode_i, "<C-j>", "<C-h>")
+local mode_n = { "n", "v" }
+local mode_v = { "v" }
+local mode_x = { "n", "x" }
+
+map(mode_x, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map(mode_x, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map(mode_x, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map(mode_x, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 map(mode_n, "<C-h>", "<C-w>h")
 map(mode_n, "<C-j>", "<C-w>j")
@@ -16,11 +17,11 @@ map(mode_n, "s", "")
 map(mode_n, ";", ":")
 
 -- close all highlight for searching results
-map(mode_n, "<leader>nh", ":nohl<CR>")
+map(mode_n, "<esc>", "<cmd>noh<cr><esc>")
 
 map("n", "<leader>sv", "<C-w>v")
 map("n", "<leader>sh", "<C-w>s")
-map("n", "<leader>sc", "<C-w>q")
+map("n", "<leader>sq", "<C-w>q")
 
 -- resize with arrows
 map("n", "<C-Up>", ":resize -2<CR>")
